4a84ab6e59b7539d7f5ac03adadae5902c4c0152
juanDcuatindioyN <126802980+juanDcuatindioyN@users.noreply.github.com>
2024-02-19 21:17:50 -0500

    persistencia

--- Directorio/src/main/java/com/mycompany/directorio/Persistencia.java
+++ Directorio/src/main/java/com/mycompany/directorio/Persistencia.java
@@ -6,6 +6,14 @@ package com.mycompany.directorio;
 
 import java.util.ArrayList;
 import com.mycompany.directorio.Contacto;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import javax.servlet.ServletContext;
 
 /**
  *
@@ -14,10 +22,50 @@ import com.mycompany.directorio.Contacto;
 public class Persistencia {
     
     ArrayList <Contacto> contactico = new ArrayList<>();
-    
-    public static void escribirContacto( ArrayList <Contacto> contactico){
-        
+  
+    public static void escribirContacto(ServletContext contexto, ArrayList<Contacto> contactico) {
+         String path= contexto.getRealPath("data.txt");
+         File arc= new File (path);
+        try {
+           
+            FileOutputStream fw = new FileOutputStream(path);
+            ObjectOutputStream pw =new ObjectOutputStream(fw);
+            pw.writeObject(contactico);
+            pw.close();
+            System.out.println("se cargo exitosamente" + path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("ha ocurrido un error" + e.getMessage());
+        }
     }
     
-    
+     public static ArrayList<Contacto> cargarContacto(ServletContext contexto) {
+        
+        ArrayList<Contacto> contactico = new ArrayList<>();
+        String p="data.txt";
+        String path= contexto.getRealPath(p);
+        File arc= new File (path);
+        System.out.println("El archivo serializado se encuentra en: "+path);
+         try {
+               FileInputStream fileIn=new FileInputStream(path);
+               ObjectInputStream ois=new ObjectInputStream(fileIn);
+             
+            
+             contactico = (ArrayList<Contacto>) ois.readObject();
+             ois.close();
+             System.out.println("Se leyo -----");
+             System.out.println("buscando en: " + path);
+         }catch (FileNotFoundException ex) {
+                
+                System.out.println("No se encontró el archivo");
+            } catch (IOException ex) {
+                
+                System.out.println("Error al leer el archivo");
+            } catch (ClassNotFoundException ex) {
+                
+                System.out.println("Clase no encontrada al deserializar");
+            }
+            
+       return contactico;
+    }
 }
